e2feea66562cbe326dde46ff8b713ae9aee3bf8c
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-02-15 19:32:02 -0600

    Add another code block

--- friends.js
+++ friends.js
@@ -1,6 +1,11 @@
 var array= [11, 21, 31, 41, 51, 61, 71, 81];
+// for (var i=0; i<array.length; i++) {
+//     if(array[i]>30) {
+//         console.log(array[i]);
+//     }
+// }
 for (var i=0; i<array.length; i++) {
-    if(array[i]>30) {
-        console.log(array[i]);
+    if(array[i]<60) {
+        console.log(array[i]); 
     }
 }
